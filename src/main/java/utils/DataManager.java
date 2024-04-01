f60a3240dd7da071fc1ef32a240f5e33bd06a7de
syoumzic <rofetofe@mail.ru>
2024-04-01 16:12:13 +0500

    чтение  файла через Files, Path утилиты

--- src/main/java/utils/DataManager.java
+++ src/main/java/utils/DataManager.java
@@ -1,6 +1,9 @@
 package utils;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 /**
@@ -13,8 +16,6 @@ public class DataManager {
      * Сохраняет в файл текущий объект Data
      */
     public void saveData(Data data) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-
         try(PrintWriter writer = new PrintWriter(new FileWriter(path))){
             for (Map.Entry<String, String> entry: data.entrySet()) {
                 String line = entry.getKey() + ":" + entry.getValue();
@@ -28,16 +29,13 @@ public class DataManager {
      */
     public Data loadData() throws IOException {
         Data data = new Data();
-        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                int pointsIndex = line.indexOf(':');
 
-                if(pointsIndex == -1) throw new IOException();
-
-                data.setString(line.substring(0, pointsIndex), line.substring(pointsIndex+1));
-            }
+        for(String line: Files.readAllLines(Paths.get(path))){
+            int pointsIndex = line.indexOf(':');
+            if(pointsIndex == -1) throw new IOException();
+            data.setString(line.substring(0, pointsIndex), line.substring(pointsIndex+1));
         }
+
         return data;
     }
 }
