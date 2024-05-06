be22374aed87df0008449d883eb483db076eb62a
syoumzic <syoumzic@gmail.com>
2024-05-06 23:04:30 +0500

    Добавлено кэширование сообщения позиции робота

--- src/main/java/gui/RobotPositionWindow.java
+++ src/main/java/gui/RobotPositionWindow.java
@@ -8,20 +8,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
 /**
  * Окно показывающее текущие координаты робота
  */
 public class RobotPositionWindow extends JInternalFrame implements PropertyChangeListener, Savable, Localizable {
-    private TextArea textArea;
-    private String positionLabel;
+    private final TextArea textArea;
+    private String positionLabel, targetLabel, angleLabel;
     private GameModel gameModel;
+    private final MessageFormat messageFormat;
 
     public RobotPositionWindow() {
         super("Позиция робота", true, true, true, true);
 
         textArea = new TextArea();
+        messageFormat = new MessageFormat("{0}:\nx: {1}\ny: {2}\n{3}:\nx: {4}\ny: {5}\n{6}:\n{7}");
 
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(textArea, BorderLayout.CENTER);
@@ -38,7 +41,18 @@ public class RobotPositionWindow extends JInternalFrame implements PropertyChang
      * Метод для обновления координат на окне
      */
     private void updatePosition() {
-        textArea.setText("%s:\nx: %f\ny: %f".formatted(positionLabel, gameModel.getRobotPositionX(), gameModel.getRobotPositionY()));
+        Object[] args = new Object[]{
+                positionLabel,
+                gameModel.getRobotPositionX(),
+                gameModel.getRobotPositionY(),
+                targetLabel,
+                gameModel.getTargetPositionX(),
+                gameModel.getTargetPositionY(),
+                angleLabel,
+                gameModel.getRobotDirection()
+        };
+
+        textArea.setText(messageFormat.format(args));
     }
 
     @Override
@@ -57,7 +71,9 @@ public class RobotPositionWindow extends JInternalFrame implements PropertyChang
     @Override
     public void onUpdateLocale(ResourceBundle bundle) {
         setTitle(bundle.getString("title"));
-        this.positionLabel = bundle.getString("positionLabel");
+        positionLabel = bundle.getString("positionLabel");
+        targetLabel = bundle.getString("targetLabel");
+        angleLabel = bundle.getString("angleLabel");
         updatePosition();
     }
 }
