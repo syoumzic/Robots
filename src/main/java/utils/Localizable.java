3348e223c8f5bcbf78dbf3888251b340580ff1de
syoumzic <syoumzic@gmail.com>
2024-05-07 15:52:02 +0500

    добавление javaDoc

--- src/main/java/utils/Localizable.java
+++ src/main/java/utils/Localizable.java
@@ -6,6 +6,13 @@ import java.util.ResourceBundle;
  * Интерфейс объектов, которые можно локализировать
  */
 public interface Localizable {
+    /**
+     * Получить имя сущности
+     */
     String getWindowName();
+
+    /**
+     * Перевести сущность по заданному ResourceBundle
+     */
     void onUpdateLocale(ResourceBundle bundle);
 }
